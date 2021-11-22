4e0c5f129b004061a1afcf5a62ef50ad5db3bf34
Lukasz Sobel <lukasz.sobel@hotmail.com>
2021-11-22 13:07:09 +0100

    add ProgramName and AboutPrograms components

---
+++ src/components/common/ProgramName/ProgramName.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../ProgramName/ProgramName.module.scss';
+
+const ProgramName = () => (
+  <div className={styles.root}>
+    <h1 className={styles.programName}>PROGRAM OPERACYJNY POMOC ŻYWNOŚCIOWA 2021</h1>
+  </div>
+);
+
+export default ProgramName;
