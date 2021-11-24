d5bd355a48dd3fd78a2a13bda9d16a6ce00ca372
Lukasz Sobel <lukasz.sobel@hotmail.com>
2021-11-24 19:08:04 +0100

    add new font and combine styles between components on main view

--- src/components/common/ProgramName/ProgramName.js
+++ src/components/common/ProgramName/ProgramName.js
@@ -3,7 +3,8 @@ import styles from '../ProgramName/ProgramName.module.scss';
 
 const ProgramName = () => (
   <div className={styles.root}>
-    <h1 className={styles.programName}>PROGRAM OPERACYJNY POMOC ŻYWNOŚCIOWA 2021</h1>
+    <h1 className={styles.programName}>program operacyjny pomoc żywnościowa 2014-2020</h1>
+    <h2 className={styles.podprogramName}>podprogram 2021</h2>
   </div>
 );
 
