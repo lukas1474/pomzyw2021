461d2c1b7f92b2034f936152c7a5c38c69938601
Karol Sobolewski <karolsobolewski92@gmail.com>
2021-11-25 19:50:43 +0100

    Modify template for components

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -6,7 +6,7 @@ import  Splash  from '../../views/Splash/Splash';
 const Header = () => (
   <header className={styles.header}>
     <Navigation />
-    <img src="/images/splash.webp" alt="splash" />
+    <img src="/images/splash4.webp" alt="splash" />
     <Splash />
   </header>
 );
