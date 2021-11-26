15bf105d9264253a509cf23cc286f1f597ce134c
Karol Sobolewski <karolsobolewski92@gmail.com>
2021-11-26 16:13:58 +0100

    Move title to splash component & Add dark overlay to splash

--- src/components/views/MainPage/MainPage.js
+++ src/components/views/MainPage/MainPage.js
@@ -2,15 +2,11 @@ import React from 'react';
 import styles from './MainPage.module.scss';
 
 import AboutProgram from '../../common/AboutProgram/AboutProgram';
-import ProgramName from '../../common/ProgramName/ProgramName';
 import Posters from '../../features/Posters/Posters';
 import ProgramDepartments from '../../common/ProgramDepartments/ProgramDepartments';
 
 const MainPage = () => (
   <div className={styles.root}>
-    <section id="program">
-      <ProgramName />
-    </section>
     <section id="oProgramie">
       <AboutProgram />
     </section>
