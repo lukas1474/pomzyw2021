460e4793700042edb6e47036736ffe06e3837437
Lukasz Sobel <lukasz.sobel@hotmail.com>
2021-11-27 18:59:59 +0100

    fix styles

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import styles from '../Footer/Footer.module.scss';
 
+import { Container, Row, Col } from 'react-bootstrap';
+
 import contact from '../../../data/contact.json';
 
 import SKlogo from '../../../posters/logo-starokatolicki.webp';
 import programLogo from '../../../posters/programLogo.webp';
 
-import { Container, Row, Col } from 'react-bootstrap';
-
 const Footer = () => (
   <footer className={styles.footer}>
     <Container>
-      <Row className={styles.abc}>
+      <Row className={styles.footerRow}>
         <Col className={styles.logo}>
-          <Col className={styles.asd}>
+          <Col className={styles.logoDetails}>
             <img className={styles.SKlogo} src={SKlogo} alt='Starokatolicki' />
           </Col>
-          <Col className={styles.asd}>
+          <Col className={styles.logoDetails}>
             <img className={styles.programLogo} src={programLogo} alt='Program logo' />
           </Col>
         </Col>
