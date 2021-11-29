5de7a0b1e402c7c7e311aa545be118a1a7f74390
Lukasz Sobel <lukasz.sobel@hotmail.com>
2021-11-29 12:00:54 +0100

    add text for about program and change tex in about us

--- src/components/common/AboutProgram/AboutProgram.js
+++ src/components/common/AboutProgram/AboutProgram.js
@@ -6,7 +6,10 @@ import aboutProgram from '../../../data/aboutProgram.json';
 const AboutProgram = () => (
   <div className={styles.root}>
     <h2 className={styles.aboutProgramTitle}>{aboutProgram.title}</h2>
-    <p className={styles.aboutProgram}>{aboutProgram.description}</p>
+    <p className={styles.aboutProgram}>{aboutProgram.description1}</p>
+    <p className={styles.aboutProgram1}>{aboutProgram.description2}</p>
+    <p className={styles.aboutProgram}>{aboutProgram.description3}</p>
+    <p className={styles.aboutProgram2}>{aboutProgram.description4}</p>
   </div>
 );
 
--- src/components/common/AboutUs/AboutUs.js
+++ src/components/common/AboutUs/AboutUs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './AboutUs.module.scss';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
 import logo from '../../../posters/pomzywlogo.webp';
 
@@ -10,16 +10,14 @@ const AboutUs = () => {
   return (
     <Container className={styles.root}>
       <h2 className={styles.aboutUsTitle}>{aboutUs.title}</h2>
-      <Row className={styles.aboutUsRow}>
-        <Col className={styles.colLogo}>
-          <img src={logo} alt="logo"/>
-        </Col>
-        <Col  className={styles.text}>
-          <p className={styles.aboutUsDesc1}>{aboutUs.description1}</p>
-          <p className={styles.aboutUsDesc1}>{aboutUs.description2}</p>
-          <p className={styles.aboutUsDesc1}>{aboutUs.description3}</p>
-        </Col>
-      </Row>
+      <div className={styles.div1}>
+        <img className={styles.descriptionLogo}src={logo} alt="logo"/>
+        <p className={styles.aboutUsDesc}>{aboutUs.description1}</p>
+        <p className={styles.aboutUsDesc}>{aboutUs.description2}</p>
+        <p className={styles.aboutUsDesc}>{aboutUs.description3}</p>
+        <p className={styles.aboutUsDesc}>{aboutUs.description4}</p>
+        <p className={styles.aboutUsDesc}>{aboutUs.description5}</p>
+      </div>
     </Container>
   );
 };
