d177b26aa2c660f37647c2e0f96e1eb1bb1e34c9
Lukasz Sobel <lukasz.sobel@hotmail.com>
2021-11-29 15:20:54 +0100

    add second logo in about us

--- src/components/common/AboutUs/AboutUs.js
+++ src/components/common/AboutUs/AboutUs.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import styles from './AboutUs.module.scss';
+
 import { Container } from 'react-bootstrap';
 
-import logo from '../../../posters/pomzywlogo.webp';
-
 import aboutUs from '../../../data/aboutUs.json';
+
+import programLogo from '../../../posters/pomzywlogo.webp';
+import starokatolickiLogo from '../../../posters/KSLogo.webp';
 
 const AboutUs = () => {
   return (
     <Container className={styles.root}>
       <h2 className={styles.aboutUsTitle}>{aboutUs.title}</h2>
-      <div className={styles.div1}>
-        <img className={styles.descriptionLogo}src={logo} alt="logo"/>
-        <p className={styles.aboutUsDesc}>{aboutUs.description1}</p>
-        <p className={styles.aboutUsDesc}>{aboutUs.description2}</p>
-        <p className={styles.aboutUsDesc}>{aboutUs.description3}</p>
-        <p className={styles.aboutUsDesc}>{aboutUs.description4}</p>
-        <p className={styles.aboutUsDesc}>{aboutUs.description5}</p>
+
+      <div className={styles.logo}>
+        <img className={styles.descriptionLogo}src={starokatolickiLogo} alt="logo"/>
+        <img className={styles.descriptionLogo}src={programLogo} alt="logo"/>
       </div>
+      <p dangerouslySetInnerHTML={{__html: aboutUs.description1}} className={styles.aboutUsDesc}/>
+      <p dangerouslySetInnerHTML={{__html: aboutUs.description2}} className={styles.aboutUsDesc}/>
+      <p dangerouslySetInnerHTML={{__html: aboutUs.description3}} className={styles.aboutUsDesc}/>
+      <p dangerouslySetInnerHTML={{__html: aboutUs.description4}} className={styles.aboutUsDesc}/>
+      <p dangerouslySetInnerHTML={{__html: aboutUs.description5}} className={styles.aboutUsDesc}/>
+
     </Container>
   );
 };
