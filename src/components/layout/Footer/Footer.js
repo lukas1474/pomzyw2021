e7b5b306e35eefe6647a88e5be23ba1f167ee318
Karol Sobolewski <karolsobolewski92@gmail.com>
2021-11-30 20:39:48 +0100

    Fix styling for footer & Fix size of menu links & Fix size of Project's Name

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import styles from '../Footer/Footer.module.scss';
 
-import { Container, Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 import contact from '../../../data/contact.json';
 
 const Footer = () => (
   <footer className={styles.root} id="kontakt">
-    <Container>
-      <Row className={styles.footerRow}>
-        <Col className={styles.logo}>
-          <Col className={styles.logoDetails}>
-            <img className={styles.SKlogo} src="/images/logo-starokatolicki__black.webp" alt='Logo Starokatolicki' />
-          </Col>
-          <Col className={styles.logoDetails}>
-            <img className={styles.programLogo} src="/images/logo-calatrava.webp" alt='Logo Program' />
-          </Col>
+    <Row className={styles.footerRow}>
+      <Col className={styles.logo}>
+        <Col className={styles.logoDetails}>
+          <img className={styles.SKlogo} src="/images/logo-starokatolicki__black.webp" alt='Logo Starokatolicki' />
         </Col>
-        <Col className={styles.contactText}>
-          <h2 className={styles.contactHead}>Kontakt:</h2>
-          <h3 className={styles.contactTitle}>{contact.title}</h3>
-          <p className={styles.contactDetails}>{contact.address}</p>
-          <p className={styles.contactDetails}>{contact.tel}</p>
-          <p className={styles.contactDetails}>{contact.mail}</p>
+        <Col className={styles.logoDetails}>
+          <img className={styles.programLogo} src="/images/logo-calatrava.webp" alt='Logo Program' />
         </Col>
-      </Row>
-    </Container>
-    <p className={styles.textFooter}>2021</p>
+      </Col>
+      <Col className={styles.contactText}>
+        <h3 className={styles.contactTitle}>{contact.title}</h3>
+        <p className={styles.contactDetails}>{contact.address}</p>
+        <p className={styles.contactDetails}>{contact.tel}</p>
+        <p className={styles.contactDetails}>{contact.mail}</p>
+      </Col>
+    </Row>
+    <div className={styles.yearBox}>
+      <h6 className={styles.textFooter}>2021</h6>
+    </div>
   </footer>
 );
 
