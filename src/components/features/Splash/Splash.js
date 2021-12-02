d4718d8c395e9ae0cfb95b8dd1139a0b3a770f19
Karol Sobolewski <karolsobolewski92@gmail.com>
2021-12-02 11:57:12 +0100

    Add dark theme support for Splash and Navigation

--- src/components/features/Splash/Splash.js
+++ src/components/features/Splash/Splash.js
@@ -10,9 +10,6 @@ const Splash = () => (
     </div>
     <div className={styles.bottomCurve}>
       <svg id="bottomCurve" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1400 150">
-        <defs>
-          <style>{`.curvePath{fill:#f0f0f0;}`}</style>
-        </defs>
         <path className="curvePath" d="M0,300H1400V150S775,389,0,150" transform="translate(0 -150)"/>
       </svg>
     </div>
