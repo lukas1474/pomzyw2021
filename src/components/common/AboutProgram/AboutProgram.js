f28742a88108e36fa861f00f325e337134e821a7
Karol Sobolewski <karolsobolewski92@gmail.com>
2021-12-03 14:54:43 +0100

    Add e-mail for aboutUs section & Modify link colors

--- src/components/common/AboutProgram/AboutProgram.js
+++ src/components/common/AboutProgram/AboutProgram.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from '../AboutProgram/AboutProgram.module.scss';
+import { Row, Col } from 'react-bootstrap';
 
 import aboutProgram from '../../../data/aboutProgram.json';
 
@@ -9,9 +10,16 @@ const AboutProgram = () => (
     <p className={styles.aboutProgram}>{aboutProgram.description1}</p>
     <p className={styles.aboutProgram1}>{aboutProgram.description2}</p>
     <p className={styles.aboutProgram}>{aboutProgram.description3}</p>
-    <p className={styles.aboutProgram2}>{aboutProgram.description4}
-      <a className={styles.aboutProgram2} href="tel:+48 696 645 486">+48 696 645 486</a>
-    </p>
+    <div className={styles.contactBox}>
+      <Col className="d-flex align-items-center justify-content-center flex-column flex-md-row pb-3 pb-md-0">
+        <p> {aboutProgram.description4}</p>
+        <a className={styles.aboutProgram2} href="tel:+48 696 645 486">+48 696 645 486</a>
+      </Col>
+      <Col className="d-flex align-items-center justify-content-center flex-column flex-md-row">
+        <p> {aboutProgram.description5}</p>
+        <a className={styles.aboutProgram2} href="mailto:kontakt@pomoczywnosciowa.pl">kontakt@pomoczywnosciowa.pl</a>
+      </Col>
+    </div>
   </div>
 );
 
