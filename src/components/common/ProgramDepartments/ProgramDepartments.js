d0f59a3dfe145ea49722716706a6da7426cd5f21
Łukasz Sobel <lukas1474@gmail.com>
2022-01-20 14:35:35 +0100

    add departments in main view

--- src/components/common/ProgramDepartments/ProgramDepartments.js
+++ src/components/common/ProgramDepartments/ProgramDepartments.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import styles from './ProgramDepartments.module.scss';
 
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import departments from '../../../data/departments.json';
 
 const ProgramDepartments = () => {
   return (
     <div className={styles.root}>
-      <h2 className={styles.title}>Gdzie szukać pomocy?</h2>
-      <Container className={styles.container}>
+      <h2 className={styles.departmentsTitle}>Gdzie szukać pomocy?</h2>
+      <div className={`container ${styles.container}`}>
         {departments.map((department, index) => {
           return(
-            <div className={styles.row} key={index}>
-              <div className={styles.department}>
-                <h5 className={styles.departmentTitle}>{department.name}</h5>
+            <div className={`row ${styles.row}`} key={index}>
+              <div className={`col-12 col-sm-12 col-lg-5 ${styles.department}`}>
+                <p className={styles.departmentsSubtitle}>{department.name}</p>
+              </div>
+              <div className={`col-12 col-sm-12 col-lg-4 ${styles.department}`}>
                 <p className={styles.departmentDetails}>{department.address}</p>
-                <p className={styles.departmentDetails}>{department.nrTel}</p>
-                <p className={styles.departmentDetails}>{department.mail}</p>
               </div>
             </div>
           );
         })}
-      </Container>
+      </div>
     </div>
   );
 };
--- src/components/views/MainPage/MainPage.js
+++ src/components/views/MainPage/MainPage.js
@@ -3,7 +3,7 @@ import styles from './MainPage.module.scss';
 
 import AboutProgram from '../../common/AboutProgram/AboutProgram';
 import Posters from '../../features/Posters/Posters';
-// import ProgramDepartments from '../../common/ProgramDepartments/ProgramDepartments';
+import ProgramDepartments from '../../common/ProgramDepartments/ProgramDepartments';
 import AboutUs from '../../common/AboutUs/AboutUs';
 
 //TODO dodać sekcję jak już będą aktualne adresy//
@@ -16,12 +16,12 @@ const MainPage = () => (
     <section className={styles.MainViewSection} id="plakaty">
       <Posters />
     </section>
+    <section className={styles.MainViewSection}  id="punktyPomocy">
+      <ProgramDepartments />
+    </section>
     <section className={styles.MainViewSection} id="oNas">
       <AboutUs />
     </section>
-    {/* <section className={styles.MainViewSection}  id="punktyPomocy">
-      <ProgramDepartments />
-    </section> */}
   </div>
 );
 
