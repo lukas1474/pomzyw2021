96250e0e809782b3cd03b460f0925ee4134259e9
Karol Sobolewski <karolsobolewski92@gmail.com>
2022-01-21 17:39:46 +0100

    Add new photo for splash & Fix navigation link for program departmets

--- src/components/common/ProgramName/ProgramName.js
+++ src/components/common/ProgramName/ProgramName.js
@@ -6,7 +6,6 @@ import programName from '../../../data/programName.json';
 const ProgramName = () => (
   <div className={styles.root}>
     <h1 className={styles.programName}>{programName.title}</h1>
-    <h2 className={styles.podprogramName}>{programName.subtitle}</h2>
   </div>
 );
 
