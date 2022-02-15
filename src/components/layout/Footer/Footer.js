1e03b5876f8edba1956e9a9f610832d209648c0a
Karol Sobolewski <karolsobolewski92@gmail.com>
2022-02-15 10:33:13 +0100

    Change Crucialfix email address

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => (
         <p className={styles.textFooter}>© 2021</p>
         <a href="https://www.starokatolicki.eu/" className={styles.textFooter}>Kościół Starokatolicki w RP</a>
       </div>
-      <p className={styles.textFooter}> Powered by <a href="mailto:crucialfix@outlook.com">CrucialFix</a> </p>
+      <p className={styles.textFooter}> Powered by <a href="mailto:info@crucialfix.pl">CrucialFix</a> </p>
     </div>
   </footer>
 );
