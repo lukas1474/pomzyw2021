bda2b1ec8eca29fd52c144f6cb62b830541e6b26
Łukasz Sobel <lukas1474@gmail.com>
2022-12-04 22:40:50 +0100

    add new files to download

--- src/components/views/ToDownload/ToDownload.js
+++ src/components/views/ToDownload/ToDownload.js
@@ -14,6 +14,26 @@ const ToDownload = () => {
                 <p>Wykaz magazynów organizacji partnerskich regionalnych w ramach Programu Operacyjnego Pomoc Żywnościowa 2014-2020 współfinansowanego z Europejskiego Funduszu Pomocy Najbardziej Potrzebującym Podprogram 2021.</p>
                 <a href="/documents/opisDzialan.pdf" download="Pomoc_zywnosciowa_opis_dzialan.pdf">Pobierz</a>
               </li>
+              <br />
+              <li>
+                <p>Sprawozdanie końcowe z przebiegu działań w ramach środków towarzyszących</p>
+                <a href="/documents/Załącznik nr 12_2021.pdf" download="Sprawozdanie końcowe z przebiegu działań w ramach środków towarzyszących - załącznik nr 12_2021.pdf">Pobierz</a>
+              </li>
+              <br />
+              <li>
+                <p>Sprawozdanie końcowe z dystrybucji artykułów spożywczych - strona 1</p>
+                <a href="/documents/Załącznik nr 11_2021-str1.pdf" download="Sprawozdanie końcowe z dystrybucji artykułów spożywczych - Załącznik nr 11_2021 - strona 1.pdf">Pobierz</a>
+              </li>
+              <br />
+              <li>
+                <p>Sprawozdanie końcowe z dystrybucji artykułów spożywczych - strona 2</p>
+                <a href="/documents/Załącznik nr 11_2021-str2.pdf" download="Sprawozdanie końcowe z dystrybucji artykułów spożywczych - Załącznik nr 11_2021 - strona 2.pdf">Pobierz</a>
+              </li>
+              <br />
+              <li>
+                <p>Sprawozdanie końcowe z dystrybucji artykułów spożywczych - strona 3 i 4</p>
+                <a href="/documents/Załącznik nr 11_2021-str 3 i 4.pdf" download="Sprawozdanie końcowe z dystrybucji artykułów spożywczych - Załącznik nr 11_2021 - strona 3 i 4.pdf">Pobierz</a>
+              </li>
             </ul>
           </div>
         </div>
